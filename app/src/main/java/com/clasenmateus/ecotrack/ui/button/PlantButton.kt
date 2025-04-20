0d63f9073cea03ec3dee09fc8b5bf0b1f970bdc9
mateus-clasen <mateushc2@hotmail.com>
2025-04-19 23:03:20 -0300

    plantButton

--- app/src/main/java/com/clasenmateus/ecotrack/ui/button/PlantButton.kt
+++ app/src/main/java/com/clasenmateus/ecotrack/ui/button/PlantButton.kt
@@ -1,9 +1,13 @@
 package com.clasenmateus.ecotrack.ui.button
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -11,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.clasenmateus.ecotrack.ui.theme.Green2
@@ -22,14 +27,19 @@ import com.clasenmateus.ecotrack.ui.theme.Typography
 @Composable
 fun PlantButton(
     modifier: Modifier = Modifier,
-    onClick: () -> Unit
+    context: Context = LocalContext.current,
+    url: String = "https://arvoredo.org.br/plante-uma-arvore/",
 ){
     Button(
         modifier = modifier.heightIn(min = 56.dp),
         shape = RoundedCornerShape(8.dp),
         contentPadding = ButtonDefaults.ContentPadding,
         colors = ButtonDefaults.buttonColors(containerColor = Green2),
-        onClick = onClick
+        onClick = {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            context.startActivity(intent)
+
+        }
     ) {
         val text = "Plante sua árvore agora"
         Text(text.uppercase(), style = Typography.labelLarge)
@@ -48,6 +58,6 @@ fun PlantButton(
 @Composable
 private fun PlantButtonPreview(){
     PlantButton(
-        modifier = Modifier.fillMaxWidth()
-    ){}
+        modifier = Modifier.fillMaxWidth().padding(top = 26.dp)
+    )
 }
